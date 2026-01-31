64e44b1461a2437826629b34379705304da5749e
Edison Pizarro Godoy <edison.pizarro@front.com>
2026-01-31 16:59:58 -0300

    Folding UFO

---
+++ lua/custom/plugins/folding.lua
@@ -0,0 +1,23 @@
+-- nvim-ufo - better folding with preview
+
+return {
+  'kevinhwang91/nvim-ufo',
+  dependencies = { 'kevinhwang91/promise-async' },
+  event = 'BufRead',
+  keys = {
+    { 'zR', function() require('ufo').openAllFolds() end, desc = 'Open all folds' },
+    { 'zM', function() require('ufo').closeAllFolds() end, desc = 'Close all folds' },
+    { 'zK', function() require('ufo').peekFoldedLinesUnderCursor() end, desc = 'Peek folded lines' },
+  },
+  opts = {
+    provider_selector = function()
+      return { 'treesitter', 'indent' }
+    end,
+  },
+  init = function()
+    vim.opt.foldcolumn = '1'
+    vim.opt.foldlevel = 99
+    vim.opt.foldlevelstart = 99
+    vim.opt.foldenable = true
+  end,
+}
