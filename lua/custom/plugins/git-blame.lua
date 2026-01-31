dc65fabed34e73d15abfaab81606f5ffb772866f
Edison Pizarro Godoy <edison.pizarro@front.com>
2026-01-31 16:46:54 -0300

    Git blame

---
+++ lua/custom/plugins/git-blame.lua
@@ -0,0 +1,12 @@
+-- Git Blame - show inline blame for each line
+
+return {
+  'f-person/git-blame.nvim',
+  event = 'VeryLazy',
+  opts = {
+    enabled = true,
+    message_template = ' <summary> • <date> • <author> • <<sha>>',
+    date_format = '%d-%m-%Y %H:%M:%S',
+    virtual_text_column = 1,
+  },
+}
